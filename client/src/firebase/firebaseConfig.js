a589b38eb30e2ce8d221b31485811cb57d4ca590
Garima Bhayana <garimaabhayanaa@gmail.com>
2025-03-23 15:23:40 +0530

    Updated firebaseConfig.js

--- client/src/firebase/firebaseConfig.js
+++ client/src/firebase/firebaseConfig.js
@@ -6,12 +6,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA_OSAJqNhQ-Ke90OQ30RGls0ZiswZy_WY",
-  authDomain: "cocreate-ed5e1.firebaseapp.com",
-  projectId: "cocreate-ed5e1",
-  storageBucket: "cocreate-ed5e1.firebasestorage.app",
-  messagingSenderId: "345112820325",
-  appId: "1:345112820325:web:5031068a5dbe7d9e5be48c"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.SENDERID,
+  appId: process.env.APP_ID
 };
 
 // Initialize Firebase
